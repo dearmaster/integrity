c7d52137ec4edd317c56ea16436a68696fc23f99
dearmaster windows7 <694114533@qq.com>
2016-10-17 01:08:37 +0800

    not yet finished.

--- crm/src/main/java/com/master/crm/action/UserAction.java
+++ crm/src/main/java/com/master/crm/action/UserAction.java
@@ -1,8 +1,14 @@
 package com.master.crm.action;
 
+import com.master.crm.service.UserService;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.log4j.Logger;
 
 public class UserAction extends ActionSupport {
+
+    private static final Logger logger = Logger.getLogger(UserAction.class);
+
+    private UserService userService;
 
     @Override
     public String execute() throws Exception {
@@ -10,11 +16,20 @@ public class UserAction extends ActionSupport {
     }
 
     public String addUser() throws Exception {
+        logger.info("add user");
         return SUCCESS;
     }
 
     public String delUser() throws Exception {
         return SUCCESS;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
 }
